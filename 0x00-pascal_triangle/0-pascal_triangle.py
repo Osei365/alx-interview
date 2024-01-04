ab08e9b52a9ece7ba8933de76711d2a1c336cddc
Osei <williamsabodunrin@gmail.com>
2024-01-04 06:24:25 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    if n > 2:
+        result = [[1], [1, 1]]
+        for i in range(1, n-1):
+            w = result[i]
+            new = [w[0]]
+            new.extend([w[e] + w[e+1] for e in range(i)])
+            new.append(w[-1])
+            result.append(new)
+        return result
