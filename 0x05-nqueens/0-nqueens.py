aa32846a931c5402e61cd746a8440ffa4fb7c1eb
Osei <williamsabodunrin@gmail.com>
2024-02-09 01:48:05 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -8,8 +8,10 @@ if len(sys.argv) != 2:
     sys.exit(1)
 if not sys.argv[1].isdigit():
     print('N must be a number')
+    sys.exit(1)
 if int(sys.argv[1]) < 4:
     print('N must be at least 4')
+    sys.exit(1)
 n = int(sys.argv[1])
 def queens(n, i=0, a=[], b=[], c=[]):
     """ find possible positions """
